41f3edf5f14d17c786f52f39c2089b8372ca10d6
ozark-dev <jason@ozarkdev.com>
2023-04-15 16:00:59 -0500

    Added the JPA query to find Students using findByLastName

--- src/main/java/com/kaminocode/cruddemo/CruddemoApplication.java
+++ src/main/java/com/kaminocode/cruddemo/CruddemoApplication.java
@@ -23,8 +23,21 @@ public class CruddemoApplication {
 
 //			readStudent(studentDAO);
 
-			queryForStudents(studentDAO);
+//			queryForStudents(studentDAO);
+
+			queryForStudentsByLastName(studentDAO);
 		};
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+		// get a list of students
+		List<Student> theStudents = studentDAO.findByLastName("Andor");
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 	private void queryForStudents(StudentDAO studentDAO) {
