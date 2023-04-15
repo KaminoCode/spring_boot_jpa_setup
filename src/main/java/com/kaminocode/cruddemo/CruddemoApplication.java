abd65ab6cf030655054797a4748e7a2a78e00195
ozark-dev <jason@ozarkdev.com>
2023-04-14 23:58:17 -0500

    Added findById method to retrieve records from the student database

--- src/main/java/com/kaminocode/cruddemo/CruddemoApplication.java
+++ src/main/java/com/kaminocode/cruddemo/CruddemoApplication.java
@@ -17,8 +17,34 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Cassian", "Andor", "cassian@kaminocode.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generate id: " + theId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		System.out.println("Found the student: " +myStudent);
+
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
--- src/main/java/com/kaminocode/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/kaminocode/cruddemo/dao/StudentDAOImpl.java
@@ -24,4 +24,11 @@ public class StudentDAOImpl implements StudentDAO{
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
     }
+
+    // implement findById method
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
 }
