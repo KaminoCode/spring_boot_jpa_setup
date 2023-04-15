3dbf61de463b18e7bfce381cc6db418b2f8f6665
ozark-dev <jason@ozarkdev.com>
2023-04-15 15:36:21 -0500

    Added the JPA query logic for findAll Students

--- src/main/java/com/kaminocode/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/kaminocode/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,13 @@ package com.kaminocode.cruddemo.dao;
 
 import com.kaminocode.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
